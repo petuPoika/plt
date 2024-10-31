1b8fa00d8d22b0584d47f5c47f6edbd9700c9f54
Petteri Castren <petu.castren@hotmail.com>
2024-10-31 16:59:18 +0200

    US1 - test 1

--- test/test_piglatin.py
+++ test/test_piglatin.py
@@ -5,5 +5,7 @@ from error import PigLatinError
 
 class TestPigLatin(unittest.TestCase):
 
-    def test_something(self):
-        pass
+    def test_set_an_input(self):
+        translator = PigLatin("hello world")
+        phrase = translator.get_phrase()
+        self.assertEqual("hello world", phrase)
